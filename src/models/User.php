10a579627bf18557b0e895e5bbb695618910c08f
agostinhodev <agostinhoneto.est@gmail.com>
2021-01-03 11:08:18 -0300

    Added a way to verify if the property exists on class in php

--- src/models/User.php
+++ src/models/User.php
@@ -4,15 +4,23 @@ namespace src\models;
 
 final class User
 {
-    private $id;
-    private $name;
-    private $password;
+    private $id = NULL;
+    private $name = NULL;
+    private $password = NULL;
 
     public function __set($atrib, $value){
+
+        if(!property_exists($this, $atrib))
+            throw new \Exception("The attribute " . $atrib . " don't exists in " . get_class());
+
         $this->$atrib = $value;
     }
 
     public function __get($atrib){
+
+        if(!property_exists($this, $atrib))
+            throw new \Exception("The attribute " . $atrib . " don't exists in " . get_class());
+
         return $this->$atrib;
     }
 
